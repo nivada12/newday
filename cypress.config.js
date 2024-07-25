aec9ef1412afcc0bef7e3d0d0765cb620acb5418
nivada12 <33197736+nivada12@users.noreply.github.com>
2024-07-25 18:28:40 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,23 @@
-module.exports = {
+// cypress.config.js
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (["chrome", "edge"].includes(browser.name)) {
+          if (browser.isHeadless) {
+            launchOptions.args.push("--no-sandbox");
+            launchOptions.args.push("--disable-gl-drawing-for-tests");
+            launchOptions.args.push("--disable-gpu");
+          }
+          launchOptions.args.push("--js-flags=--max-old-space-size=3500");
+        }
+        return launchOptions;
+      });
     },
   },
-};
+});
